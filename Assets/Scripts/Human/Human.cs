bb1b26aed9ee4decce5b17affae4e8f9128bdf43
rafal1253 <rafal1253@gmail.com>
2023-07-14 15:05:15 +0200

    Refactor prefab folder

---
+++ Assets/Scripts/Human/Human.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    enum State { Idle, Walk, Carry}
+    State _state;
+
+    Transform _target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
