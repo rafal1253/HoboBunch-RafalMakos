604c3889e8fba717cf3059823c427dd59ea1a458
rafal1253 <rafal1253@gmail.com>
2023-07-14 16:00:55 +0200

    Add NavMesh to human and building

--- Assets/Scripts/Human/Human.cs
+++ Assets/Scripts/Human/Human.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Human : MonoBehaviour
 {
@@ -9,15 +10,31 @@ public class Human : MonoBehaviour
 
     Transform _target;
 
+    NavMeshAgent _agent;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveByClick();
+    }
+
+
+    void MoveByClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+                _agent.SetDestination(hit.point); 
+        }
     }
 }
